d8525a61e78a661e008b0117a09de0d084e70a43
khj33 <hyunjo111@naver.com>
2023-09-08 14:56:46 +0900

    프로세스 종료 수정

--- 근무관리 회피용 마우스 이동 프로그램/final_mouse_move.py
+++ 근무관리 회피용 마우스 이동 프로그램/final_mouse_move.py
@@ -4,6 +4,7 @@ import pyautogui
 import time
 import threading
 import random
+import psutil   # 실행중인 프로세스 및 시스템 활용 라이브러리
 
 
 class mouse_Move() :
@@ -73,6 +74,23 @@ class mouse_Move() :
         
     def quit(self) :
         self.window.destroy()
+        for proc in psutil.process_iter():
+            try:
+                # 프로세스 이름, PID값 가져오기
+                processName = proc.name()
+                processID = proc.pid
+                #print(processName , ' - ', processID)
+        
+                if processName == "khj_mouse.exe":
+                    #print(processName , ' - ', processID)
+                    parent_pid = processID  #PID
+                    parent = psutil.Process(parent_pid) # PID 찾기
+                    for child in parent.children(recursive=True):  #자식-부모 종료
+                        child.kill()
+                    parent.kill()
+        
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):   #예외처리
+                pass
         
         
         
